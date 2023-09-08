6c5426b74dc103df5f6607454eea05ddf0c04ae6
NunzioDiCroce <nunziodavide@gmail.com>
2023-09-08 12:27:19 +0200

    Create project

---
+++ FastOutBackEnd/src/main/java/com/example/FastOutBackEnd/FastOutBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.FastOutBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastOutBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastOutBackEndApplication.class, args);
+	}
+
+}
---
+++ FastOutBackEnd/src/test/java/com/example/FastOutBackEnd/FastOutBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FastOutBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastOutBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
