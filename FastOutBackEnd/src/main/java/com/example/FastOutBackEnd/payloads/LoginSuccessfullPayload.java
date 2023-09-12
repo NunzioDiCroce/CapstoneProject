7d4a8a3f81d83e9bc37665531e6d8f7dfe5dd1b7
NunzioDiCroce <nunziodavide@gmail.com>
2023-09-12 16:55:23 +0200

    Create AuthController.java

---
+++ FastOutBackEnd/src/main/java/com/example/FastOutBackEnd/payloads/LoginSuccessfullPayload.java
@@ -0,0 +1,14 @@
+package com.example.FastOutBackEnd.payloads;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class LoginSuccessfullPayload {
+	
+	String accessToken;
+
+}
---
+++ FastOutBackEnd/src/main/java/com/example/FastOutBackEnd/payloads/UserLoginPayload.java
@@ -0,0 +1,11 @@
+package com.example.FastOutBackEnd.payloads;
+
+import lombok.Getter;
+
+@Getter
+public class UserLoginPayload {
+	
+	String email;
+	String password;
+
+}
