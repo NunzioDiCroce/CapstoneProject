e29e90ce5cc24fcf32f2a33dfed648d9d17a850c
NunzioDiCroce <nunziodavide@gmail.com>
2023-09-15 18:02:03 +0200

    Update resources.component

--- FastOutFrontEnd/FastOutFrontEndApp/src/app/components/resources/resources.component.ts
+++ FastOutFrontEnd/FastOutFrontEndApp/src/app/components/resources/resources.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+import { Resource } from 'src/app/models/resource.interface';
+import { ResourcesService } from 'src/app/services/resources.service';
+import { AuthData } from 'src/app/auth/auth-data.interface';
+import { AuthService } from 'src/app/auth/auth.service';
+import { Subscription } from 'rxjs';
+//import { Router } from '@angular/router';
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 @Component({
   selector: 'app-resources',
   templateUrl: './resources.component.html',
@@ -7,9 +16,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResourcesComponent implements OnInit {
 
-  constructor() { }
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  user!: AuthData | null;
+  resources: Resource[] | undefined;
+  sub!: Subscription;
+
+  constructor( private resourcesSrv:ResourcesService, private authSrv:AuthService ) { }
 
   ngOnInit(): void {
+
+    this.authSrv.user$.subscribe((_user) => {
+      this.user = _user;
+      console.log(this.user)
+    });
+
+    this.sub = this.resourcesSrv.getResources().subscribe((_resources:Resource[]) => {
+      this.resources = _resources;
+      console.log(this.resources)
+    });
+
   }
+
+  ngOnDestroy():void {
+
+    if(this.sub) {
+      this.sub.unsubscribe()
+    }
+
+  }
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 
 }
--- FastOutFrontEnd/FastOutFrontEndApp/src/app/services/resources.service.ts
+++ FastOutFrontEnd/FastOutFrontEndApp/src/app/services/resources.service.ts
@@ -1,18 +1,35 @@
 import { Injectable } from '@angular/core';
 
-// interface import
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 import { Resource } from '../models/resource.interface';
-
-// import HttpClient to manage http methods
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs';
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResourcesService {
 
-  constructor( private http:HttpClient ) { } // HttpCLient parameter declaration
 
-  // TODO: custom methods definition
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  constructor( private http:HttpClient ) { }
+
+  getResources() {
+    const userString = localStorage.getItem('user');
+
+    if (!userString) {
+      return this.http.get<Resource[]>('http://localhost:3001/resources');
+    }
+
+    const user = JSON.parse(userString);
+    const token = user.accessToken;
+
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+
+    return this.http.get<any>('http://localhost:3001/resources', { headers }).pipe(map(response => response.content))
+  }
 
 }
