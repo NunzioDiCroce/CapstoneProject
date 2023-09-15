38d142f74f4a4b75e008504a23809ac975ea3816
NunzioDiCroce <nunziodavide@gmail.com>
2023-09-15 19:06:19 +0200

    Update home.components (work in progress)

--- FastOutFrontEnd/FastOutFrontEndApp/src/app/components/home/home.component.ts
+++ FastOutFrontEnd/FastOutFrontEndApp/src/app/components/home/home.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+import { AuthData } from 'src/app/auth/auth-data.interface';
+import { AuthService } from 'src/app/auth/auth.service';
+//import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +14,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  user!: AuthData | null;
+
+  constructor( private router: Router, private authSrv:AuthService ) { }
 
   ngOnInit(): void {
+
+    this.authSrv.user$.subscribe((_user) => {
+      this.user = _user;
+      console.log(this.user)
+    });
+
   }
+
+  goToLogin() {
+    this.router.navigate(['login'])
+  }
+
+  goToRegister() {
+    this.router.navigate(['register'])
+  }
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 
 }
