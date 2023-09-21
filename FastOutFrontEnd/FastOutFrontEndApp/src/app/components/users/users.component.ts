327adaf40ca8a0d71c9d8e9365caa80af3614d32
NunzioDiCroce <nunziodavide@gmail.com>
2023-09-21 19:01:00 +0200

    Update users.component

--- FastOutFrontEnd/FastOutFrontEndApp/src/app/components/users/users.component.ts
+++ FastOutFrontEnd/FastOutFrontEndApp/src/app/components/users/users.component.ts
@@ -51,15 +51,11 @@ export class UsersComponent implements OnInit {
     });
   }
 
-  // table row number
-  increaseRowNumber() {
-    this.currentRowNumber++;
-  }
-
   // pagination
   prevPage(): void {
     if (this.currentPage > 0) {
       this.currentPage--;
+      this.currentRowNumber = this.currentPage * this.pageSize + 1;
       this.loadUsers();
     }
   }
@@ -68,6 +64,7 @@ export class UsersComponent implements OnInit {
   goToPage(page: number): void {
     if (page >= 0 && page < this.totalPages) {
       this.currentPage = page;
+      this.currentRowNumber = this.currentPage * this.pageSize + 1;
       this.loadUsers();
     }
   }
@@ -76,6 +73,7 @@ export class UsersComponent implements OnInit {
   nextPage(): void {
     if (this.currentPage < this.totalPages - 1) {
       this.currentPage++;
+      this.currentRowNumber = this.currentPage * this.pageSize + 1;
       this.loadUsers();
     }
   }
