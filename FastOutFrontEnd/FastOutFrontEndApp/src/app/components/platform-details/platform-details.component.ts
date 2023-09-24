b55397aabfa12e8ec15ea823b1d7b52553382593
NunzioDiCroce <nunziodavide@gmail.com>
2023-09-24 17:55:14 +0200

    Complete platform-details.component.ts

--- FastOutFrontEnd/FastOutFrontEndApp/src/app/components/platform-details/platform-details.component.ts
+++ FastOutFrontEnd/FastOutFrontEndApp/src/app/components/platform-details/platform-details.component.ts
@@ -11,6 +11,7 @@ import { Subscription } from 'rxjs';
 import { Router, ActivatedRoute } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { Resource } from 'src/app/models/resource.interface';
+import { Equipment } from 'src/app/models/equipment.interface';
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 @Component({
@@ -21,72 +22,80 @@ import { Resource } from 'src/app/models/resource.interface';
 export class PlatformDetailsComponent implements OnInit {
 
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-user!: AuthData | null;
-sub!: Subscription;
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  user!: AuthData | null;
+  sub!: Subscription;
 
-platformDetails: PlatformDetails | null = null;
+  platformDetails: PlatformDetails | null = null;
 
-resourcesForPlatform: Resource[] = [];
+  resourcesForPlatform: Resource[] = [];
+  equipmentsForPlatform: Equipment[] = [];
 
-constructor( private platformsSrv:PlatformsService, private authSrv:AuthService, private router: Router, private route: ActivatedRoute ) { }
+  constructor( private platformsSrv: PlatformsService, private authSrv: AuthService, private router: Router, private route: ActivatedRoute ) { }
 
-ngOnInit(): void {
-  this.authSrv.user$.subscribe((_user) => {
-    this.user = _user;
-    console.log(this.user)
-  });
+  ngOnInit(): void {
+    this.authSrv.user$.subscribe((_user) => {
+      this.user = _user;
+      console.log(this.user)
+    });
 
-  // platformId recovery from the URL
-  this.route.paramMap.subscribe((params) => {
-    const platformId = params.get('id')!;
-    this.loadPlatformDetails(platformId);
-  });
-}
-
-loadPlatformDetails(platformId: string): void {
-  this.sub = this.platformsSrv.getPlatformDetails(platformId).subscribe((details: PlatformDetails) => {
-    this.platformDetails = details;
-    this.loadResourcesForPlatform(platformId);
-  });
-}
-
-loadResourcesForPlatform(platformId: string) {
-  this.platformsSrv.getResourcesForPlatform(platformId).subscribe(resourcesForPlatform => {
-    this.resourcesForPlatform = resourcesForPlatform;
-  });
-}
-
-deletePlatform(): void {
-  if (!this.platformDetails || !this.platformDetails.id) {
-    return;
+    // platformId recovery from the URL
+    this.route.paramMap.subscribe((params) => {
+      const platformId = params.get('id')!;
+      this.loadPlatformDetails(platformId);
+    });
   }
-  // check whether resources and/or equipment are associated with the platform before deleting it
-  if (this.resourcesForPlatform.length > 0) {
-    alert("Before deleting the platform it is necessary to remove the resources and equipment associated with it.");
-    return;
+
+  loadPlatformDetails(platformId: string): void {
+    this.sub = this.platformsSrv.getPlatformDetails(platformId).subscribe((details: PlatformDetails) => {
+      this.platformDetails = details;
+      this.loadResourcesForPlatform(platformId);
+      this.loadEquipmentsForPlatform(platformId)
+    });
   }
-  const platformId = this.platformDetails.id;
-  this.platformsSrv.deletePlatform(platformId).subscribe(
-    () => {
-      this.router.navigate(['/platforms']);
-    },
-    (error) => {
-      console.error('Error deleting platform:', error);
+
+  loadResourcesForPlatform(platformId: string) {
+    this.platformsSrv.getResourcesForPlatform(platformId).subscribe(resourcesForPlatform => {
+      this.resourcesForPlatform = resourcesForPlatform;
+    });
+  }
+
+  loadEquipmentsForPlatform(platformId: string) {
+    this.platformsSrv.getEquipmentsForPlatform(platformId).subscribe(equipmentsForPlatform => {
+      this.equipmentsForPlatform = equipmentsForPlatform;
+    });
+  }
+
+  deletePlatform(): void {
+    if (!this.platformDetails || !this.platformDetails.id) {
+      return;
     }
-  );
-}
-
-navigateBack(): void {
-  this.router.navigate(['/platforms']);
-}
-
-ngOnDestroy():void {
-  if(this.sub) {
-    this.sub.unsubscribe()
+    // check whether resources and/or equipments are associated with the platform before deleting it
+    if (this.resourcesForPlatform.length > 0 || this.equipmentsForPlatform.length > 0) {
+      window.alert("Before deleting the platform it is necessary to remove resources and equipments associated with it.");
+      return;
+    }
+    const platformId = this.platformDetails.id;
+    this.platformsSrv.deletePlatform(platformId).subscribe(
+      () => {
+        this.router.navigate(['/platforms']);
+      },
+      (error) => {
+        console.error('Error deleting platform:', error);
+      }
+    );
   }
-}
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  navigateBack(): void {
+    this.router.navigate(['/platforms']);
+  }
+
+  ngOnDestroy():void {
+    if(this.sub) {
+      this.sub.unsubscribe()
+    }
+  }
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
 }
