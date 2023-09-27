b4f8d1ea68d06427055572c3d1c7fb4fece5237a
NunzioDiCroce <nunziodavide@gmail.com>
2023-09-27 12:47:04 +0200

    Update resources.component

--- FastOutFrontEnd/FastOutFrontEndApp/src/app/components/resources/resources.component.ts
+++ FastOutFrontEnd/FastOutFrontEndApp/src/app/components/resources/resources.component.ts
@@ -104,6 +104,16 @@ export class ResourcesComponent implements OnInit {
     }
   }
 
+  // to customize Euro currency format
+  euroCurrencyFormat(value: number): string {
+    const formattedValue = new Intl.NumberFormat('it-IT', {
+      style: 'currency',
+      currency: 'EUR',
+      minimumFractionDigits: 2
+    }).format(value);
+    return formattedValue;
+  }
+
   ngOnDestroy():void {
     if(this.sub) {
       this.sub.unsubscribe()
