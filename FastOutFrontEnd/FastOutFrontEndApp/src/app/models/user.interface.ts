461d7ae8f9e996accfe26a6a00c08917566a0daf
NunzioDiCroce <nunziodavide@gmail.com>
2023-09-28 17:00:44 +0200

    Update user.interface.ts

--- FastOutFrontEnd/FastOutFrontEndApp/src/app/models/user.interface.ts
+++ FastOutFrontEnd/FastOutFrontEndApp/src/app/models/user.interface.ts
@@ -1,15 +1,10 @@
 export interface User {
 
-    // interface properties definition
-    id?: string,
-
-    name: string,
-    surname: string,
-
-    email: string,
-
-    password: string,
-
-    role: string
+  id?: string,
+  name: string,
+  surname: string,
+  email: string,
+  password: string,
+  role: string
 
 }
